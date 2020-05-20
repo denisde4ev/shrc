615a8311db180d3ae2c0bc284620db6c965e67a4
Denis Dechev (u0_a99) <deni2020@abv.bg>
2020-05-20 04:05:09 +0300

    todo

--- ln(~\bashrc->.).sh
+++ ln(~\bashrc->.).sh
@@ -1,5 +1,8 @@
 #! /bin/bash -x
 
+echo 'TODO: compare to ./ln(.->~) and FIX ME'
+exit 111
+
 for i in ~/.bashrc*
-do [[ -f $i ]] &&  ln -- "$i" "./$(basename "$i")"
+do [[ -f $i ]] &&  ln -sr -- "$i" "./$(basename "$i")"
 done
