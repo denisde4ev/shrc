bb968fb79ad08655cdda49875532f43fc150e092
Denis Dechev <denisde4ev@vivaldi.net>
2022-01-14 00:08:44 +0200

    2022-01-14

--- _fetch/_fetch.sh
+++ _fetch/_fetch.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/ /hint/sh
+${shell_is_interactive-return}
 
-( cd ~/B/_fetch && . "$(printf '%s\n' ./[!_.%]*[!%~] | shuf -n 1)" ) 
+( cd ~/B/_fetch && . "$(printf '%s\n' ./[!_.%]*[!%~] | shuf -n 1)" >&2 0<&- )
