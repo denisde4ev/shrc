b7cdfd873620816c95203297927a89b2febda8cf
Denis Dechev <denisde4ev@vivaldi.net>
2022-05-17 04:43:58 +0300

    2022-05-17

--- __sourceable/pop_to_i.sh
+++ __sourceable/pop_to_i.sh
@@ -1,27 +1,25 @@
 #! /hint/sh
-{
-	unset i
-	i=$#
 
-	case $i in
-	0)
-		return 1
-		;;
-	???*)
-		eval \
-			"i=\${$i};" \
-			"set -- $(seq "$(( i - 1 ))" | sed 's/^/"\${/;  s/$/}" \\/; $ a;')" \
-		&& \
-		[ $# -eq $(( i - 1 )) ]
-	;;
-	*) 
-		while case $i in 1) false; esac; do
-			set -- "$@" "$1"
-			shift
-			i=$(( i - 1 ))
-		done
-		i=$1; shift
-	;;
-	esac
+unset i
+i=$#
 
-}
+case $i in
+0)
+	return 1
+	;;
+???*)
+	eval \
+		"i=\${$i};" \
+		"set -- $(seq "$(( i - 1 ))" | sed 's/^/"\${/;  s/$/}" \\/; $ a;')" \
+	&& \
+	[ $# -eq $(( i - 1 )) ]
+;;
+*) 
+	while case $i in 1) false; esac; do
+		set -- "$@" "$1"
+		shift
+		i=$(( i - 1 ))
+	done
+	i=$1; shift
+;;
+esac
--- __sourceable/reverse_args.sh
+++ __sourceable/reverse_args.sh
@@ -1,7 +1,14 @@
+#! /hint/sh
 
-reverse_args_first=''
-for reverse_args_i; do
-	set -- "$reverse_args_i" ${reverse_args_first-"$@"}
-	unset reverse_args_first
-done
-unset reverse_args_i reverse_args_first
+case $# in
+???*)
+	eval "set -- $(seq "$#" | tac | sed 's/^/"\${/;  s/$/}" \\/; $ a;')"
+	;;
+*)
+	reverse_args_first=''
+	for reverse_args_i; do
+		set -- "$reverse_args_i" ${reverse_args_first-"$@"}
+		unset reverse_args_first
+	done
+	unset reverse_args_i reverse_args_first
+esac
