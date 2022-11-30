78d07c3145a6add710dcc42f913d9595b12083f2
denisde4ev <git@denisde4ev.xyz>
2022-11-30 23:40:40 +0200

    c

--- _loadable/history_current_command.sh
+++ _loadable/history_current_command.sh
@@ -1,26 +1,26 @@
 
 history_current_command() {
-		local i
+	#	unset-unseted-i # unset-unseted-i # DO NOT, or will UNEST `I` IN EVERY LINE
+	local i || :
 
-		#i=$(
-		#	set -fue
-		#	IFS=''
-		#	history | {
-		#		unset r
-		#		while read -r i; do r=$i; done
-		#		puts "$r"
-		#	}
-		#) || return
-		i=$(history | tail -n 1) # seems to be faster for mine conf: `HISTSIZE=500000;HISTFILESIZE=1000000`
+	#i=$(
+	#	set -fue
+	#	IFS=''
+	#	history | {
+	#		unset r
+	#		while read -r i; do r=$i; done
+	#		puts "$r"
+	#	}
+	#) || return
+	i=$(history | tail -n 1) # seems to be faster for mine conf: `HISTSIZE=500000;HISTFILESIZE=1000000`
 
-		# handle format s/^ *[0-9]* *//
-		i=${i#"${i%%[! ]*}"}   # trim start spaces
-		i=${i#"${i%%[!0-9]*}"} # trim start digits
-		i=${i#"${i%%[! ]*}"}   # trim start spaces (again)
+	# handle format s/^ *[0-9]* *//
+	i=${i#"${i%%[! ]*}"}   # trim start spaces
+	i=${i#"${i%%[!0-9]*}"} # trim start digits
+	i=${i#"${i%%[! ]*}"}   # trim start spaces (again)
 
-		# Not needed: case $# in 1) i=${};; 0) ;; *) err .....
-		i=${i#"$1"}
-		# i=${i#"${i%%[! ]*}"}   # trim start spaces (again x2) just in case
+	# Not needed: case $# in 1) i=${};; 0) ;; *) throw err .....
+	i=${i#"$1"}
 
-		puts "$i"
+	puts "$i"
 }
