51f5f2d91dbcb17c4b539f5df6b21c42f3de0391
denisde4ev <git@denisde4ev.xyz>
2025-02-17 05:00:03 +0200

    c

--- _loadable/arg.bash
+++ _loadable/arg.bash
@@ -2,10 +2,14 @@
 arg() {
 	if [[ -t 1 && -t 2 ]]; then
 		local IFS=''
-		unset-unseted-i
+		case ${shell_supports_local+x} in x);; *)
+			unset-unseted-i
+		esac
 		local i; i=$*
 		printf %s "$#:${#i} " >&2;
-		unset-seted-i
+		case ${shell_supports_local+x} in x);; *)
+			unset-seted-i
+		esac
 	fi
 	echo "${@@Q}"
 }
